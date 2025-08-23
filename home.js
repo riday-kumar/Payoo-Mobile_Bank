ea08031ce9eabd17b90be7e49528f5f0d120c6a9
riday-kumar <hridoykumarsaha07@gmail.com>
2025-08-24 00:29:25 +0600

    cash out and toggle feature added

--- home.js
+++ home.js
@@ -1,9 +1,10 @@
 const validPin = 1234;
+let mainAmount = parseInt(document.getElementById("main-amount").innerText);
+
+// add money Feature
 document.getElementById("addMoneyBtn").addEventListener("click", function (e) {
   e.preventDefault();
 
-  //   main amount
-  let mainAmount = parseInt(document.getElementById("main-amount").innerText);
   //   get input from user
   const selectedBank = document.getElementById("select-bank").value;
   const bankAcNum = document.getElementById("bank-ac-num").value;
@@ -13,7 +14,7 @@ document.getElementById("addMoneyBtn").addEventListener("click", function (e) {
 
   //   Validation
   if (bankAcNum.length != 11) {
-    console.log(bankAcNum.length + "," + pinNumber.length);
+    // console.log(bankAcNum.length + "," + pinNumber.length);
     alert("Bank Account Number Should Be 11 digit");
     return;
   }
@@ -27,4 +28,53 @@ document.getElementById("addMoneyBtn").addEventListener("click", function (e) {
   const afterAddedMoney = mainAmount + amountToAdd;
   //   set new money
   document.getElementById("main-amount").innerText = afterAddedMoney;
+});
+
+// Toggle Feature in various button
+// three section in Individual variable
+const addMoneyFormSec = document.getElementById("addMoneySec");
+const addCashOutFormSec = document.getElementById("cashOutSec");
+const addTranferMoneyFormSec = document.getElementById("transferMoneySec");
+
+document.getElementById("add-money-btn").addEventListener("click", function () {
+  addMoneyFormSec.style.display = "block";
+  addCashOutFormSec.style.display = "none";
+  addTranferMoneyFormSec.style.display = "none";
+});
+
+document.getElementById("cah-out-btn").addEventListener("click", function () {
+  addMoneyFormSec.style.display = "none";
+  addCashOutFormSec.style.display = "block";
+  addTranferMoneyFormSec.style.display = "none";
+});
+
+document
+  .getElementById("transfer-money-btn")
+  .addEventListener("click", function () {
+    addMoneyFormSec.style.display = "none";
+    addCashOutFormSec.style.display = "none";
+    addTranferMoneyFormSec.style.display = "block";
+  });
+
+// Cash Out feature
+document.getElementById("cashOutBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("xyz");
+  const agentNumber = parseInt(document.getElementById("agent-ac-num").value);
+  const cashAmount = parseInt(
+    document.getElementById("amount-for-cashout").value
+  );
+  const pinNumberCashOut = parseInt(
+    document.getElementById("pin-number-cash-out").value
+  );
+
+  if (pinNumberCashOut !== validPin) {
+    alert("Pin Number is not Correct");
+    return;
+  }
+
+  mainAmount -= cashAmount;
+
+  document.getElementById("main-amount").innerText = mainAmount;
+  document.getElementById("amount-for-cashout").value = "";
 });
